35834a02e632e1f19dd9995c2090e34f4a6cfebb
Cheryl Danner <cdanner@exptechinc.com>
2017-11-08 18:20:37 -0500

    allow variable batch sizes.

--- models/transform_nets.py
+++ models/transform_nets.py
@@ -11,7 +11,7 @@ def input_transform_net(point_cloud, is_training, bn_decay=None, K=3):
     """ Input (XYZ) Transform Net, input is BxNx3 gray image
         Return:
             Transformation matrix of size 3xK """
-    batch_size = point_cloud.get_shape()[0].value
+    # batch_size = point_cloud.get_shape()[0].value
     num_point = point_cloud.get_shape()[1].value
 
     input_image = tf.expand_dims(point_cloud, -1)
@@ -30,7 +30,7 @@ def input_transform_net(point_cloud, is_training, bn_decay=None, K=3):
     net = tf_util.max_pool2d(net, [num_point,1],
                              padding='VALID', scope='tmaxpool')
 
-    net = tf.reshape(net, [batch_size, -1])
+    net = tf.reshape(net, [-1, 1024])
     net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training,
                                   scope='tfc1', bn_decay=bn_decay)
     net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training,
@@ -48,7 +48,7 @@ def input_transform_net(point_cloud, is_training, bn_decay=None, K=3):
         transform = tf.matmul(net, weights)
         transform = tf.nn.bias_add(transform, biases)
 
-    transform = tf.reshape(transform, [batch_size, 3, K])
+    transform = tf.reshape(transform, [-1, 3, K])
     return transform
 
 
@@ -56,7 +56,7 @@ def feature_transform_net(inputs, is_training, bn_decay=None, K=64):
     """ Feature Transform Net, input is BxNx1xK
         Return:
             Transformation matrix of size KxK """
-    batch_size = inputs.get_shape()[0].value
+    # batch_size = inputs.get_shape()[0].value
     num_point = inputs.get_shape()[1].value
 
     net = tf_util.conv2d(inputs, 64, [1,1],
@@ -74,7 +74,7 @@ def feature_transform_net(inputs, is_training, bn_decay=None, K=64):
     net = tf_util.max_pool2d(net, [num_point,1],
                              padding='VALID', scope='tmaxpool')
 
-    net = tf.reshape(net, [batch_size, -1])
+    net = tf.reshape(net, [-1, 1024])
     net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training,
                                   scope='tfc1', bn_decay=bn_decay)
     net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training,
@@ -91,5 +91,5 @@ def feature_transform_net(inputs, is_training, bn_decay=None, K=64):
         transform = tf.matmul(net, weights)
         transform = tf.nn.bias_add(transform, biases)
 
-    transform = tf.reshape(transform, [batch_size, K, K])
+    transform = tf.reshape(transform, [-1, K, K])
     return transform
